6d4c8a55be6467eb28f6ff30ec037273272b14c2
Tadas Ziemys <tadas@Tadass-MacBook-Pro.local>
2014-07-31 20:24:33 +0300

    Initial Commit

---
+++ MistikA/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MistikA
+//
+//  Created by Tadas Ziemys on 31/07/14.
+//  Copyright (c) 2014 Tadas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
